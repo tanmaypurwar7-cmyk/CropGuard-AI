49d4851c4e1a0f4d89930eae3796bd51fa631d98
Tanmay Purwar <tanmaypurwar7@gmail.com>
2026-08-27 22:03:36 +0530

    Removed unwanted comments for better understanding

--- app.py
+++ app.py
@@ -13,23 +13,23 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict_disease():
 
-    # Check whether an image was uploaded
+   
     if "image" not in request.files:
         return "No image uploaded"
 
     file = request.files["image"]
 
-    # Check whether a file was actually selected
+   
     if file.filename == "":
         return "No image selected"
 
-    # Open the uploaded image
+ 
     image = Image.open(file.stream)
 
-    # Send image to our AI model
+   
     results = predict(image)
 
-    # Send prediction back to webpage
+   
     return render_template(
         "result.html",
         results=results
